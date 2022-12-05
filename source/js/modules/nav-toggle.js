e6127d6ab80ec4db276e06b42ca91b0f23b94b10
Сергей Субботин <zarodiny@yandex.ru>
2022-12-05 22:46:22 +1000

    добавляет стили header

--- source/js/modules/nav-toggle.js
+++ source/js/modules/nav-toggle.js
@@ -4,7 +4,16 @@ const initNavToggle = () => {
   const button = document.querySelector('.nav__button');
   button.addEventListener('click', () => {
     nav.classList.toggle('is-active');
+    button.classList.toggle('is-active');
   });
-}
+
+  window.addEventListener('resize', () => {
+    const bodyWidth = innerWidth;
+    if (bodyWidth > 768) {
+      nav.classList.remove('is-active');
+      button.classList.remove('is-active');
+    }
+  }, false);
+};
 
 export { initNavToggle }
