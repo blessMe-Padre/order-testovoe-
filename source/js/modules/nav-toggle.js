a3c038601a1b8bd492e67023e32ea3d3ed5bdfc9
Сергей Субботин <zarodiny@yandex.ru>
2022-12-08 22:33:42 +1000

    добавляет проверки

--- source/js/modules/nav-toggle.js
+++ source/js/modules/nav-toggle.js
@@ -1,22 +1,24 @@
 const initNavToggle = () => {
   const nav = document.querySelector('.nav');
   const body = document.querySelector('.page__body');
+  if (nav) {
+    const button = document.querySelector('.nav-button');
+    button.addEventListener('click', () => {
+      nav.classList.toggle('is-active');
+      button.classList.toggle('is-active');
+      body.classList.toggle('lock');
+    });
 
-  const button = document.querySelector('.nav-button');
-  button.addEventListener('click', () => {
-    nav.classList.toggle('is-active');
-    button.classList.toggle('is-active');
-    body.classList.toggle('lock');
-  });
+    window.addEventListener('resize', () => {
+      const bodyWidth = innerWidth;
+      if (bodyWidth > 768) {
+        nav.classList.remove('is-active');
+        button.classList.remove('is-active');
+        body.classList.remove('lock');
+      }
+    }, false);
+  }
 
-  window.addEventListener('resize', () => {
-    const bodyWidth = innerWidth;
-    if (bodyWidth > 768) {
-      nav.classList.remove('is-active');
-      button.classList.remove('is-active');
-      body.classList.remove('lock');
-    }
-  }, false);
 };
 
 export { initNavToggle }
--- source/js/modules/popup.js
+++ source/js/modules/popup.js
@@ -5,38 +5,39 @@ const initPopup = () => {
   const popup = document.querySelector('.popup');
   const popupBody = document.querySelector('.popup__body');
   const body = document.querySelector('.page__body');
-
   const openBtn = document.querySelector('.order__btn');
 
-  openBtn.addEventListener('click', () => {
-    popup.classList.add('is-active');
-    body.classList.add('lock');
-  });
+  if (popup) {
+    openBtn.addEventListener('click', () => {
+      popup.classList.add('is-active');
+      body.classList.add('lock');
+    });
 
-  closeBtn.addEventListener('click', () => {
-    popup.classList.remove('is-active');
-    body.classList.remove('lock');
-  });
+    closeBtn.addEventListener('click', () => {
+      popup.classList.remove('is-active');
+      body.classList.remove('lock');
+    });
 
-  // закрываем окно по esc
-  window.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Esc' || evt.key === 'Escape') {
-      if (popup.classList.contains('is-active')) {
-        evt.preventDefault();
+    // закрываем окно по esc
+    window.addEventListener('keydown', function (evt) {
+      if (evt.key === 'Esc' || evt.key === 'Escape') {
+        if (popup.classList.contains('is-active')) {
+          evt.preventDefault();
+          popup.classList.remove('is-active');
+          body.classList.remove('lock');
+        }
+      }
+    });
+
+    // закрываем окно по клику вне попапа
+
+    popup.addEventListener('click', function (evt) {
+      if (evt.target === popupBody) {
         popup.classList.remove('is-active');
         body.classList.remove('lock');
       }
-    }
-  });
-
-  // закрываем окно по клику вне попапа
-
-  popup.addEventListener('click', function (evt) {
-    if (evt.target === popupBody) {
-      popup.classList.remove('is-active');
-      body.classList.remove('lock');
-    }
-  });
+    });
+  }
 };
 
 export { initPopup }
